15521fd7c502a798b4a9a355dabdb7a6c91ebfc2
Carlos Carvajal <cxcarvaj@espol.edu.ec>
2023-01-13 09:51:59 -0500

    First commit of the Gif Expert App

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,13 @@
+export const GifExpertApp = () => {
+    return (
+        <>
+            {/* Title */}
+            <h1>GifExpertApp</h1>
+            {/* Input */}
+
+            {/* List of Gif */}
+                {/* Gif Item */}
+
+        </>
+    );
+};
